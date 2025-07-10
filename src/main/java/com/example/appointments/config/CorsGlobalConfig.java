6b8ca8fac527c5637f08dc35298d226270147661
lazoryks <lazoriksua@gmail.com>
2025-07-10 15:19:15 +0100

    CorsGlobalConfig

---
+++ src/main/java/com/example/appointments/config/CorsGlobalConfig.java
@@ -0,0 +1,16 @@
+package com.example.appointments.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsGlobalConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://glamlimerick.com")
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
