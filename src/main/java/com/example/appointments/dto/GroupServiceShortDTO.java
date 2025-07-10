3cd850fbf258819811f8e51ec2a0b31dee750b09
lazoryks <lazoriksua@gmail.com>
2025-07-10 15:39:36 +0100

    Add /api/groups/list method

--- src/main/java/com/example/appointments/dto/GroupServiceShortDTO.java
+++ src/main/java/com/example/appointments/dto/GroupServiceShortDTO.java
@@ -1,19 +1,15 @@
 package com.example.appointments.dto;
 
+import lombok.Getter;
+
+@Getter
 public class GroupServiceShortDTO {
-    private Integer id;
-    private String groupName;
+    private final Integer id;
+    private final String groupName;
 
     public GroupServiceShortDTO(Integer id, String groupName) {
         this.id = id;
         this.groupName = groupName;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public String getGroupName() {
-        return groupName;
-    }
 }
--- src/main/java/com/example/appointments/repository/GroupServiceRepository.java
+++ src/main/java/com/example/appointments/repository/GroupServiceRepository.java
@@ -2,21 +2,13 @@ package com.example.appointments.repository;
 
 import com.example.appointments.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
+//import java.util.Map;
 
 @Repository
 public interface GroupServiceRepository extends JpaRepository<GroupService, Integer> {
-    //@Query("SELECT DISTINCT new map(g.groupId as id, g.groupName as name) FROM GroupService g")
-    //List<Map<String, Object>> findDistinctGroups();
-
-    //@Query("SELECT DISTINCT new map(g.id as id, g.groupName as name) FROM GroupService g")
-    //List<Map<String, Object>> findDistinctGroups();
-
     List<GroupService> findAll();
-
-    //List<GroupService> findByGroupId(Integer id);
 }
